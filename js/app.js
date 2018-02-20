a3ea5ba086c0ef852fb1874869cf17368d15af06
betsyvies <ing.betsyvies@gmail.com>
2018-02-19 23:57:37 -0500

    cambiando estilos

--- js/app.js
+++ js/app.js
@@ -1,3 +1,58 @@
 $(document).ready(() => {
+  var $imagesPortfolio = $('.images-portfolio');
+  var $dataModal = $('a[data-toggle=modal]');
   
+  $imagesPortfolio.on('click', function(event) {
+    /* Validamos si el atributo de la imagen es igual a un valor del array de objetos data */
+    var name = $(this).attr('alt');
+    event.preventDefault();
+  
+    /* Validamos el texto y mostramos las imagenes */
+    data.forEach(element => {
+      if (name === element.portfolio.name) {
+
+        /* Cambiamos agregamos los otros atributos a las imagenes */
+        $imagesPortfolio.attr('data-title', name);
+        $imagesPortfolio.attr('data-description', element.portfolio.description);
+        $imagesPortfolio.attr('data-objective', element.portfolio.objective);
+        $imagesPortfolio.attr('data-technology', element.portfolio['used technology']);
+        $imagesPortfolio.attr('data-members', element.portfolio.members);
+        $imagesPortfolio.attr('data-href', element.portfolio.href);
+      }
+    });
+  });
+  /* Eventos para mostrar información del restaurante */
+  /* Al pasar el mouse por la imagen se mostrará el valor de data-title */
+  $dataModal
+    .on('mouseover', function() {
+      var dataTitle = $(this).children('img').attr('alt'); 
+      console.log(dataTitle);
+        
+      $(this).children('p').text(dataTitle);
+      $(this).children('img').addClass('container-hover');
+    })
+    
+    .on('mouseout', function() {
+      var dataTitle = $(this).children('img').attr('data-title'); 
+    
+      $(this).children('p').text('');
+      $(this).children('img').removeClass('container-hover');
+    });
+  
+  /* Al darle click a la imagen se mostrará un modal */
+  $dataModal.on('click', function() {
+    var dataTitle = $(this).children().attr('data-title');
+    var dataDescription = $(this).children().attr('data-description');
+    var dataObjective = $(this).children().attr('data-objective');
+    var dataTechnology = $(this).children().attr('data-technology');
+    var dataMembers = $(this).children().attr('data-members');
+    var dataHref = $(this).children().attr('data-href');
+              
+    $($(this).data('target') + ' .modal-header h1').text(dataTitle);
+    $($(this).data('target') + ' .modal-body #description').text(dataDescription);
+    $($(this).data('target') + ' .modal-body #objective').text(dataObjective);
+    $($(this).data('target') + ' .modal-body #technology').text(dataTechnology);
+    $($(this).data('target') + ' .modal-body #members').text(dataMembers);
+    $($(this).data('target') + ' .modal-body #link-github').attr('href', dataHref);
+  });
 });
---
+++ js/data-base.js
@@ -0,0 +1,41 @@
+var data = [
+  {
+    'portfolio': {
+      'name': 'Always for komrads',
+      'description': 'Always For Komrads (AFK) es una red social para gamers, en la cual podrás postear, subir vídeos e imágenes, ver perfiles y comunicarte con ellos para tener una partida',
+      'objective': 'AFK es un proyecto desarrollado en el bootcamp de Laboratoria. El reto fue crear una red social la cual tenga perfiles de usuarios, newsfeed donde puedes ver las actualizaciones de todos tus contactos, subir fotos  y modo friendly, además de usar firebase para administrar los datos de usuarios',
+      'used technology': ['user-research', 'html5', 'css3', 'bootstrap3', 'javascript', 'jquery', 'firebase'],
+      'members': ['Betsy Vidal', 'Francesca Quezada'],
+      'href': 'https://github.com/betsyvies/afk'
+    }
+  },
+  {
+    'portfolio': {
+      'name': 'Insta collage',
+      'description': 'Insta Collage es una página, en la cual usted podrá crear collages personalizados. Sólo inicie sesión con su cuenta de gamil o regístrate.',
+      'objective': 'IC es un proyecto desarrollado en el bootcamp de Laboratoria. El reto fue crear un app para hacer collages usando la api Drag and Drop de HTML5, para arrastrar y soltar las imagenes.',
+      'used technology': ['html5', 'css3', 'bootstrap3', 'javascript', 'jquery', 'firebase'],
+      'members': ['Araceli Cueva', 'Betsy Vidal', 'Melissa Yauri'],
+      'href': 'https://github.com/betsyvies/insta-collage'
+    }
+  },
+  {
+    'portfolio': {
+      'name': 'Food map',
+      'description': 'Esta página le ayudará a buscar los mejores restaurantes cerca de usted, bajo estos cinco tipos de comida (comida china,comida sana, comida criolla, comida rapida, comida mexicana). ',
+      'objective': 'Food map es un proyecto desarrollado en el bootcamp de Laboratoria. El reto consistia en filtar a través de un input los restaurantes que se encuentran cerca de usted',
+      'used technology': ['html5', 'css3', 'bootstrap3', 'javascript', 'jquery'],
+      'href': 'https://github.com/betsyvies/food-map'
+    }
+  },
+  {
+    'portfolio': {
+      'name': 'Swapi',
+      'description': 'Swapi es una página para ver divertidas imágenes y gifs de los personajes de Starwars, como también los datos de ellos.',
+      'objective': 'Swapi es un proyecto desarrollado en el bootcamp de Laboratoria. El reto consistia en mostrar imagenes de Starwars y sus datos, esto usando las APIS Star Wars y Giphy',
+      'used technology': ['html5', 'css3', 'bootstrap3', 'javascript', 'jquery', 'APIS'],
+      'members': ['Araceli Cueva', 'Betsy Vidal', 'Melissa Yauri'],
+      'href': 'https://github.com/betsyvies/api-swapi'
+    }
+  },
+];
